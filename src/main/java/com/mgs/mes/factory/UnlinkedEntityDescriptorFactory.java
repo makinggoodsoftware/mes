e56e955b5268069e5c5f09dbf968e5304fe94c14
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2015-02-26 07:52:22 +0000

    WIP adding relationships logic. Small reorg and renaming around the MongoContextFactory

--- src/main/java/com/mgs/mes/factory/UnlinkedEntityDescriptorFactory.java
+++ src/main/java/com/mgs/mes/factory/UnlinkedEntityDescriptorFactory.java
@@ -4,34 +4,29 @@ import com.mgs.mes.db.MongoDao;
 import com.mgs.mes.db.MongoPersister;
 import com.mgs.mes.db.MongoRetriever;
 import com.mgs.mes.model.builder.EntityBuilderFactory;
-import com.mgs.mes.model.builder.RelationshipBuilderFactory;
 import com.mgs.mes.model.data.ModelData;
 import com.mgs.mes.model.data.ModelDataBuilderFactory;
 import com.mgs.mes.model.entity.Entity;
 import com.mgs.mes.model.entity.EntityBuilder;
-import com.mgs.mes.model.entity.RelationshipBuilder;
 import com.mgs.mes.model.entity.Relationships;
 import com.mgs.mes.model.factory.ModelFactory;
-import com.mgs.mes.model.relationships.RelationshipsFactory;
 import com.mgs.reflection.BeanNamingExpert;
 import com.mgs.reflection.FieldAccessorParser;
 
-import java.util.Map;
-
-public class MongoFactory {
+public class UnlinkedEntityDescriptorFactory {
 	private final MongoInternalDependencies mongoInternalDependencies;
 	private final MongoDao mongoDao;
 
-	public MongoFactory(MongoInternalDependencies mongoInternalDependencies, MongoDao mongoDao) {
+	public UnlinkedEntityDescriptorFactory(MongoInternalDependencies mongoInternalDependencies, MongoDao mongoDao) {
 		this.mongoInternalDependencies = mongoInternalDependencies;
 		this.mongoDao = mongoDao;
 	}
 
-	public <T extends Entity> MongoRetriever<T> retriever(Class<T> retrieveType) {
+	private <T extends Entity> MongoRetriever<T> retriever(Class<T> retrieveType) {
 		return new MongoRetriever<>(mongoInternalDependencies.getMongoEntities(), mongoDao, mongoInternalDependencies.getDBOModelFactory(), retrieveType);
 	}
 
-	public <T extends Entity, Z extends EntityBuilder<T>> MongoPersister<T, Z> persister(Class<T> persistType, Class<Z> updaterType) {
+	private <T extends Entity, Z extends EntityBuilder<T>> MongoPersister<T, Z> persister(Class<T> persistType, Class<Z> updaterType) {
 		EntityBuilderFactory<T, Z> tzEntityBuilderFactory = new EntityBuilderFactory<>(
 				mongoInternalDependencies.getModelDataBuilderFactory(),
 				mongoInternalDependencies.getFieldAccessorParser(),
@@ -43,7 +38,7 @@ public class MongoFactory {
 		return new MongoPersister<>(tzEntityBuilderFactory, mongoDao, mongoInternalDependencies.getMongoEntities());
 	}
 
-	public <T extends Entity, Z extends EntityBuilder<T>> EntityBuilderFactory<T, Z> builder(Class<T> typeOfModel, Class<Z> typeOfBuilder) {
+	private <T extends Entity, Z extends EntityBuilder<T>> EntityBuilderFactory<T, Z> builder(Class<T> typeOfModel, Class<Z> typeOfBuilder) {
 		ModelDataBuilderFactory modelDataBuilderFactory = mongoInternalDependencies.getModelDataBuilderFactory();
 		FieldAccessorParser fieldAccessorParser = mongoInternalDependencies.getFieldAccessorParser();
 		BeanNamingExpert beanNamingExpert = mongoInternalDependencies.getBeanNamingExpert();
@@ -51,11 +46,11 @@ public class MongoFactory {
 		return new EntityBuilderFactory<>(modelDataBuilderFactory, fieldAccessorParser, beanNamingExpert, modelDataModelFactory, typeOfModel, typeOfBuilder);
 	}
 
-	public <T extends Entity, Y extends Relationships<T>> RelationshipsFactory<T, Y> relationships(
-			Class<Y> typeOfRelationships,
-			Map<Class<? extends RelationshipBuilder>, RelationshipBuilderFactory> modelBuildersByType
-	) {
-		return new RelationshipsFactory<>(typeOfRelationships, modelBuildersByType);
+	public <T extends Entity, Z extends EntityBuilder<T>, Y extends Relationships<T>> UnlinkedEntityDescriptor<T, Z, Y>
+	create (Class<T> entityType, Class<Z> entityBuilderType, Class<Y> relationshipsType) {
+		MongoRetriever<T> retriever = retriever(entityType);
+		MongoPersister<T, Z> persister = persister(entityType, entityBuilderType);
+		EntityBuilderFactory<T, Z> builder = builder(entityType, entityBuilderType);
+		return new UnlinkedEntityDescriptor<>(retriever, persister, builder, relationshipsType, entityBuilderType);
 	}
-
 }
